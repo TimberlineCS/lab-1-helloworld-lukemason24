23c478c2908d2fbe49d49b5244f89a9deee7cc66
lukemason24 <lukemasonmd@gmail.com>
2020-08-21 04:06:05 +0000

    Finished Thing

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+//Luke Mason
+//AP Comp Sci
+//8/21/20
+public class HelloWorld{ 
+  public static void main(String args[]){
+    System.out.println("Emoji");
+    System.out.println(" ------------- ");
+    System.out.println("-              -");
+    System.out.println("-  --     --   -");
+    System.out.println("-      -       -");
+    System.out.println("- -          - -");
+    System.out.println("-  ---------   -");
+    System.out.println(" ------------- ");
+  }
+}
